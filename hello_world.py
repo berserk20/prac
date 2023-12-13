3b321910fbd1a5e69ef9d9eeaed4a576dd8aad43
berserk20 <ditiss2123@gmail.com>
2023-12-13 20:17:52 +0530

    Program that displays who u are

---
+++ hello_world.py
@@ -0,0 +1,13 @@
+name = input("Enter Your Name: ")
+age = input("Enter your age: ")
+prof = input ("Enter your profession: ")
+
+print('\n')
+
+def personal_info():
+	print("This is my Introduction,")
+	print("I'm "+name+" \nI'm "+ str(age)+" year old"+"\nI'm "+prof)
+
+personal_info()
+
+
