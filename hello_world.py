bf064369f6bbdbec17ff17485878b8798bd3c225
berserk20 <ditiss2123@gmail.com>
2023-12-15 12:08:59 +0530

    from branch test

--- hello_world.py
+++ hello_world.py
@@ -1,12 +1,14 @@
+from datetime import datetime
 name = input("Enter Your Name: ")
-age = input("Enter your age: ")
+age = input("Enter yob: ")
 prof = input ("Enter your profession: ")
-
+conv = int(age)
+current_year= datetime.today().year
 print('\n')
-
+present_age = current_year-conv
 def personal_info():
 	print("This is my Introduction,")
-	print("I'm "+name+" \nI'm "+ str(age)+" year old"+"\nAnd i do is "+prof)
+	print("I'm "+name+" \nI'm "+ str(present_age)+" year old"+"\nAnd i do is "+prof)
 
 personal_info()
 
